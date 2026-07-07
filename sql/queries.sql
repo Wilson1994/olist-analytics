a5328e29cae461aeb984943cc99828d598c56fe3
Wilson1994 <vilhovsky@gmail.com>
2026-07-07 14:56:29 +0200

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -16,3 +16,32 @@ LEFT JOIN product_category_name_translation t USING (product_category)
 LEFT JOIN olist_order_payments_dataset op USING (order_id)
 LEFT JOIN olist_order_reviews_dataset r USING (order_id)
 WHERE o.order_status = 'delivered';
+
+-- --------------------------------------------------------
+
+
+-- Місячний виторг і кількість замовлень
+SELECT
+	strftime('%Y-%m', o.order_purchase_t) AS ym,
+    ROUND(SUM(oi.price), 2) AS revenue,
+    COUNT(DISTINCT o.order_id) AS orders
+FROM olist_orders_dataset o
+JOIN olist_order_items_dataset oi USING (order_id)
+WHERE o.order_status = 'delivered'
+GROUP BY ym
+ORDER BY ym;
+
+-- --------------------------------------------------------
+
+-- топ-10 категорій за виторгом;
+SELECT 
+	t.product_category_1 AS category_en,
+    ROUND(SUM(oi.price), 2) AS revenue
+FROM olist_order_items_dataset oi
+JOIN olist_orders_dataset o USING (order_id)
+JOIN olist_products_dataset p USING (product_id)
+LEFT JOIN product_category_name_translation t USING (product_category)
+WHERE o.order_status = 'delivered'
+GROUP BY category_en
+ORDER BY revenue DESC
+LIMIT 10;
